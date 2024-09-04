a9c4418900e7bc92ea1eb540a0b6a8c77b89815e
Bernardo <bernardocar1206@gmail.com>
2024-09-04 15:51:54 -0300

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,44 +1,42 @@
-package aula2;
-
 import java.util.HashMap;
 
-public class HashMap {
+public class Principal {
 
-public static void main(String[] args) {
-		
-		// Cria um HashMap
-		HashMap<String, String> myMap = new HashMap<>();
-		// Cria chaves e seus respectivos valores
-		myMap.put("Nome", "João");
-		myMap.put("Idade", "18");
-		myMap.put("CPF", "123.456.789-01");
-		myMap.put("Sexo", "Masculino");
-		myMap.put("Raça", "Branco");
-		
-		// Fornece todas as chaves e valores da lista
-		System.out.println("Lista completa: " + myMap);
-		// Mostra o valor da chave "Idade"
-		System.out.println("Idade: " + myMap.get("Idade"));
-		
-		// Remove a chave "Nome"
-		myMap.remove("Nome");
-		
-		// Verifica se a lista está vazia
-		System.out.println("A lista está vazia? " + myMap.isEmpty());
-		
-		// Mostra quantas chaves a lista possui
-		System.out.println("Tamanho da lista: " + myMap.size());
-		
-		// Exemplo que apresenta as chaves que compoem
-		// a lista
-		for (String i : myMap.keySet()) {
-			System.out.println("Chave: " + i);
+	public static void main(String[] args) {
+			
+			// Cria um HashMap
+			HashMap<String, String> myMap = new HashMap<>();
+			// Cria chaves e seus respectivos valores
+			myMap.put("Nome", "João");
+			myMap.put("Idade", "18");
+			myMap.put("CPF", "123.456.789-01");
+			myMap.put("Sexo", "Masculino");
+			myMap.put("Raça", "Branco");
+			
+			// Fornece todas as chaves e valores da lista
+			System.out.println("Lista completa: " + myMap);
+			// Mostra o valor da chave "Idade"
+			System.out.println("Idade: " + myMap.get("Idade"));
+			
+			// Remove a chave "Nome"
+			myMap.remove("Nome");
+			
+			// Verifica se a lista está vazia
+			System.out.println("A lista está vazia? " + myMap.isEmpty());
+			
+			// Mostra quantas chaves a lista possui
+			System.out.println("Tamanho da lista: " + myMap.size());
+			
+			// Exemplo que apresenta as chaves que compoem
+			// a lista
+			for (String i : myMap.keySet()) {
+				System.out.println("Chave: " + i);
+			}
+			// Exemplo que apresenta os valores que compoem
+			// a lista
+			for (String i : myMap.values()) {
+				System.out.println("Valor: " + i);
+			}
 		}
-		// Exemplo que apresenta os valores que compoem
-		// a lista
-		for (String i : myMap.values()) {
-			System.out.println("Valor: " + i);
-		}
+		
 	}
-	
-}
